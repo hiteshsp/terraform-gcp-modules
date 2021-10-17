641b76145f76dc5c0967f7106b59296a7e544e22
Niharika <sp@Niharikas-MacBook-Air.local>
2021-10-17 14:12:26 +0530

    fixed conflicts

--- kubernetes-engine/cluster.tf
+++ kubernetes-engine/cluster.tf
@@ -1,18 +1,9 @@
 resource "google_container_cluster" "this" {
-<<<<<<< HEAD
-  name     = var.google_container_cluster_name
-  location = var.location
-  remove_default_node_pool = true
-  initial_node_count       = var.initial_node_count
-  min_master_version = var.min_master_version
-  description = var.description
-=======
   name                     = var.cluster_name
   location                 = var.location
   remove_default_node_pool = true
   initial_node_count       = var.initial_node_count
   min_master_version       = var.min_master_version
   description              = var.description
->>>>>>> 4d8da0a (Initial Commit)
 }
 
